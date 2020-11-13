64e9d3b9028c710a3744cf75939b94ca8fafed6c
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-11-13 16:45:00 +0530

    update

--- database.php
+++ database.php
@@ -7,12 +7,12 @@ $dbUser = 'root';
 $dbPass = '';
 $dbName = 'courier_db';
 
-$dbConn = mysql_connect ($dbHost, $dbUser, $dbPass) or die ('MySQL connect failed. ' . mysql_error());
-mysql_select_db($dbName) or die('Cannot select database. ' . mysql_error());
+$dbConn = ($GLOBALS["___mysqli_ston"] = mysqli_connect($dbHost,  $dbUser,  $dbPass)) or die ('mysql connect failed. ' . mysqli_error($GLOBALS["___mysqli_ston"]));
+mysqli_select_db($GLOBALS["___mysqli_ston"], $dbName) or die('Cannot select database. ' . mysqli_error($GLOBALS["___mysqli_ston"]));
 
 function dbQuery($sql)
 {
-	$result = mysql_query($sql) or die(mysql_error());
+	$result = mysqli_query($GLOBALS["___mysqli_ston"], $sql) or die(mysqli_error($GLOBALS["___mysqli_ston"]));
 	
 	return $result;
 }
@@ -21,40 +21,40 @@ function dbAffectedRows()
 {
 	global $dbConn;
 	
-	return mysql_affected_rows($dbConn);
+	return mysqli_affected_rows($dbConn);
 }
 
-function dbFetchArray($result, $resultType = MYSQL_NUM) {
-	return mysql_fetch_array($result, $resultType);
+function dbFetchArray($result, $resultType = MYSQLI_NUM) {
+	return mysqli_fetch_array($result,  $resultType);
 }
 
 function dbFetchAssoc($result)
 {
-	return mysql_fetch_assoc($result);
+	return mysqli_fetch_assoc($result);
 }
 
 function dbFetchRow($result) 
 {
-	return mysql_fetch_row($result);
+	return mysqli_fetch_row($result);
 }
 
 function dbFreeResult($result)
 {
-	return mysql_free_result($result);
+	return ((mysqli_free_result($result) || (is_object($result) && (get_class($result) == "mysqli_result"))) ? true : false);
 }
 
 function dbNumRows($result)
 {
-	return mysql_num_rows($result);
+	return mysqli_num_rows($result);
 }
 
 function dbSelect($dbName)
 {
-	return mysql_select_db($dbName);
+	return mysqli_select_db($GLOBALS["___mysqli_ston"], $dbName);
 }
 
 function dbInsertId()
 {
-	return mysql_insert_id();
+	return ((is_null($___mysqli_res = mysqli_insert_id($GLOBALS["___mysqli_ston"]))) ? false : $___mysqli_res);
 }
 ?>
